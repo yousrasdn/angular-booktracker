e3d9415c8bdcbbb26ff4dc939b52e775c40294cb
Yousra Saidani <yousaidani@gmail.com>
2019-06-24 11:53:20 -0400

    Adding more crud operations on the service for the book resource

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
@@ -7,7 +7,6 @@ import { allBooks, allReaders } from '../data';
 import { LoggerService } from './logger.service';
 import { Reader } from '../models/reader';
 import { Book } from '../models/book';
-import { BookTrackerError } from '../models/bookTrackerError';
 
 @Injectable()
 export class DataService {
@@ -36,5 +35,25 @@ export class DataService {
 
   getBookById(id: number): Observable<Book> {
     return this.httpClient.get<Book>(`/api/books/${id}`);
+  }
+
+  addBook(newBook: Book): Observable<Book> {
+    return this.httpClient.post<Book>('/api/books', newBook, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
+
+  updateBook(bookToUpdate: Book): Observable<void> {
+    return this.httpClient.put<void>(`/api/books/${bookToUpdate.bookID}`, bookToUpdate, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    });
+  }
+
+  deleteBook(bookID: number): Observable<void> {
+    return this.httpClient.delete<void>(`/api/books/${bookID}`);
   }
 }
